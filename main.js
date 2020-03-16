d20adaa616a6f9ad389466702a6cc17566ec2c46
MasatakaKudou <b1017027@fun.ac.jp>
2020-03-16 15:15:07 +0900

    [add]要素の追加が出来た

---
+++ main.js
@@ -0,0 +1,45 @@
+var STORAGE_KEY = 'todos-vuejs-demo'
+var todoStorage = {
+  fetch: function () {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach(function (todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
+
+new Vue({
+  el: '#app',
+  data: {
+    todos: []
+  },
+  watch: {
+    todos: {
+      handler: function (todos) {
+        todoStorage.save(todos)
+      },
+      deep: true
+    }
+  },
+  methods: {
+    doAdd: function (event, value) {
+      var comment = this.$refs.comment
+      if (!comment.value.length) {
+        return
+      }
+      this.todos.push({
+        id: todoStorage.uid++,
+        comment: comment.value,
+        state: 0
+      })
+      comment.value = ''
+    }
+  }
+})
