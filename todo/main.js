7f57e380f7098ff6bc822858fd0e0a028c74b587
MasatakaKudou <b1017027@fun.ac.jp>
2020-03-27 06:43:02 +0900

    [add]theadとルートインスタンスの作成、ローカルストレージへの保存と取得を記述

--- todo/main.js
+++ todo/main.js
@@ -1,95 +1,35 @@
+// ローカルストレージAPIの使用
 var STORAGE_KEY = 'todos-vuejs-demo'
 var todoStorage = {
+  // saveで格納したデータを取り出している
   fetch: function () {
     var todos = JSON.parse(
       localStorage.getItem(STORAGE_KEY) || '[]'
     )
+    // todos内の要素を1つずつ取り出してtodoとしている
     todos.forEach(function (todo, index) {
+      // indexが0からの連番が設定されるので、todo.idに設定している
       todo.id = index
     })
+    // todosの長さ、つまりリストの件数をtodoStorage.uidとして保持している
     todoStorage.uid = todos.length
     return todos
   },
+  // localStorageにtodosをJSON形式で格納する、これによってブラウザをリロードしてもデータを保持しておける
   save: function (todos) {
+    // key(STORAGE_KEY)とvalue(JSON文字列に変換したtodos)をペアにしている
     localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
   }
 }
 
-new Vue({
+// ルートインスタンスを作成
+const app = new Vue({
+  // DOM要素のオブジェクトかCSSセレクタで指定
   el: '#app',
+  // 利用するデータを定義、プロパティと呼ばれる
   data: {
-    // ★STEP5 localStorage から 取得した ToDo のリスト
-    todos: [],
-    // ★STEP11 抽出しているToDoの状態
-    current: -1,
-    // ★STEP11＆STEP13 各状態のラベル
-    options: [
-      { value: -1, label: 'すべて' },
-      { value: 0, label: '作業中' },
-      { value: 1, label: '完了' }
-    ]
   },
-  computed: {
-    // ★STEP12
-    computedTodos: function () {
-      return this.todos.filter(function (el) {
-        return this.current < 0 ? true : this.current === el.state
-      }, this)
-    },
-    // ★STEP13 作業中・完了のラベルを表示する
-    labels() {
-      return this.options.reduce(function (a, b) {
-        return Object.assign(a, { [b.value]: b.label })
-      }, {})
-      // キーから見つけやすいように、次のように加工したデータを作成
-      // {0: '作業中', 1: '完了', -1: 'すべて'}
-    }
-  },
-  // ★STEP8
-  watch: {
-    // オプションを使う場合はオブジェクト形式にする
-    todos: {
-      // 引数はウォッチしているプロパティの変更後の値
-      handler: function (todos) {
-        todoStorage.save(todos)
-      },
-      // deep オプションでネストしているデータも監視できる
-      deep: true
-    }
-  },
-  // ★STEP9
-  created() {
-    // インスタンス作成時に自動的に fetch() する
-    this.todos = todoStorage.fetch()
-  },
+  // データの変更やサーバーにHTTPリクエストを送る際に用いる
   methods: {
-    // ★STEP7 ToDo 追加の処理
-    doAdd: function (event, value) {
-      // ref で名前を付けておいた要素を参照
-      var comment = this.$refs.comment
-      // 入力がなければ何もしないで return
-      if (!comment.value.length) {
-        return
-      }
-      // { 新しいID, コメント, 作業状態 }
-      // というオブジェクトを現在の todos リストへ push
-      // 作業状態「state」はデフォルト「作業中=0」で作成
-      this.todos.push({
-        id: todoStorage.uid++,
-        comment: comment.value,
-        state: 0
-      })
-      // フォーム要素を空にする
-      comment.value = ''
-    },
-    // ★STEP10 状態変更の処理
-    doChangeState: function (item) {
-      item.state = item.state ? 0 : 1
-    },
-    // ★STEP10 削除の処理
-    doRemove: function (item) {
-      var index = this.todos.indexOf(item)
-      this.todos.splice(index, 1)
-    }
   }
 })
